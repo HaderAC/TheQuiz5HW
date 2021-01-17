c45a2d8444df365b560e4e7aafda31795bbc46f1
HaderCore <Hadercortes@gmail.com>
2021-01-16 19:35:02 -0500

    added answers to variables as well

--- quiz.js
+++ quiz.js
@@ -9,7 +9,7 @@ var questions = [
         choice2: "something2",
         choice3: "something3",
         choice4: "something4",
-      
+        answer: 1,
     },
     {
         question: "JAVASCRIPT QUESTION?",
@@ -17,7 +17,7 @@ var questions = [
         choice2: "CSS2",
         choice3: "CSS3",
         choice4: "CSS4",
-       
+        answer: 4,
     },
     {
         question: "JQUERY QUESTION?",
@@ -25,6 +25,6 @@ var questions = [
         choice2: "JQUERY2",
         choice3: "JQUERY3",
         choice4: "JQUERY4",
-      
+        answer: 4,
     },
 ];
