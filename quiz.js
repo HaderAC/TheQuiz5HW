a06d6a23142e880c9689c22e9d12a9e3e870102f
HaderCore <Hadercortes@gmail.com>
2021-01-16 19:30:30 -0500

    variables that will hold the questions

---
+++ quiz.js
@@ -0,0 +1,30 @@
+
+
+
+
+var questions = [
+    {
+        question: "HTML QUESTION?",
+        choice1: "something1",
+        choice2: "something2",
+        choice3: "something3",
+        choice4: "something4",
+      
+    },
+    {
+        question: "JAVASCRIPT QUESTION?",
+        choice1: "CSS1",
+        choice2: "CSS2",
+        choice3: "CSS3",
+        choice4: "CSS4",
+       
+    },
+    {
+        question: "JQUERY QUESTION?",
+        choice1: "JQUERY1",
+        choice2: "JQUERY2",
+        choice3: "JQUERY3",
+        choice4: "JQUERY4",
+      
+    },
+];
