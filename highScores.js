502d2ecf136604c6e8d6f4db4ca787c89d68bcdc
HaderCore <Hadercortes@gmail.com>
2021-01-16 20:11:05 -0500

    added local storage to save score

---
+++ highScores.js
@@ -0,0 +1,9 @@
+var highScoresList = document.getElementById('highScoresList');
+var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+
+
+highscores.innerHTML = highscores
+.map(score => {
+    return '<li class="high-scores">${username.score}-${score.score}</li>';
+})
+.join("");
