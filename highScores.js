cb01fc82d4b8021d8b197fb7f25a2302478062df
HaderCore <Hadercortes@gmail.com>
2021-01-25 12:01:39 -0500

    changed pathway to fix deplyment issue

--- highScores.js
+++ highScores.js
@@ -1,9 +1,13 @@
 var highScoresList = document.getElementById('highScoresList');
 var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
 
+$(".saveBtn").on("click", function () {
+
 
 highscores.innerHTML = highscores
 .map(score => {
     return '<li class="high-scores">${username.score}-${score.score}</li>';
 })
-.join("");
+
+
+});
